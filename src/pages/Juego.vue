8bca49b2ce2a6184f8de14b997b3fbce98307797
jisbruzzi <jose.sbru@gmail.com>
2020-09-12 16:19:03 -0300

    Agregar cositos en los textos

--- src/pages/Juego.vue
+++ src/pages/Juego.vue
@@ -8,7 +8,7 @@
                 v-ripple 
                 v-for="opcion in opciones" 
                 :key="opcion.id" 
-                @click="elegirOpcion(opcion)"
+                @click.native="elegirOpcion(opcion.id)"
                 >
                 <q-item-section>{{opcion.descripcion}}</q-item-section>
             </q-item>
@@ -18,30 +18,44 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import { QForm } from 'quasar'
-
+import textosReal from './textos.json'
+import {formatear} from "./formateador"
+import { format } from 'path';
+let textos:any = textosReal;
 @Component({
 })
 export default class Juego extends Vue {
-    titulo="holis"
-    contenido="holis\nholis holis holis"
-    opciones=[
-        {
-            id:1,
-            descripcion:"ir a 1"
-        },
-        {
-            id:2,
-            descripcion:"ir a dos"
-        },
-        {
-            id:3,
-            descripcion:"ir a 3333333333"
-        }
-    ]
-    elegirOpcion(opcion:any){
-        this.$router.push("/juego/asdasd")
+    get id():string{
+        return this.historial[0] || "0";
+    }
+    get titulo(){
+        return formatear((textos as any)[this.id]["titulo"],this.nombre)
+    }
+    get contenido(){
+        return formatear((textos as any)[this.id]["descripcion"],this.nombre)
+    }
+    get opciones(){
+        let opciones = (textos as any)[this.id]["opciones"]
+        return (opciones || []).map( (o:any)=>({
+            id:o.id,
+            descripcion:formatear(o.descripcion,this.nombre)
+        }));
+    }
+    @Prop() forma!:number;
+    @Prop() vidaDeOracion!:number;
+    @Prop() nombre!:string;
+    @Prop() historial!:Array<string>;
+
+
+    elegirOpcion(id:string){
+        this.$router.push({ path: '/juego', query: {
+          forma: this.forma+"",
+          vidaDeOracion: this.vidaDeOracion+"",
+          nombre: this.nombre,
+          historial: [ id, ...this.historial ]
+        }})
     }
 }
 </script>
---
+++ src/pages/formateador.ts
@@ -0,0 +1,22 @@
+import personasReal from './personas.json'
+let personas:Array<string> = personasReal;
+function persona(n:number,seed:string):string{
+    let seedTotal:number=0;
+    for (let index = 0; index < seed.length; index++) {
+        seedTotal+=seed.charCodeAt(index)
+    }
+    let nDefinitivo=(seedTotal+n) % personas.length
+    return personas[nDefinitivo];
+
+}
+function formatear(texto:string,miNombre:string):string{
+    console.log(texto);
+    return texto
+        .replace("{{p1}}",persona(1,miNombre))
+        .replace("{{p2}}",persona(2,miNombre))
+        .replace("{{p3}}",persona(3,miNombre))
+        .replace("{{p4}}",persona(4,miNombre))
+        .replace("{{nombre}}",miNombre)
+}
+
+export { formatear }
